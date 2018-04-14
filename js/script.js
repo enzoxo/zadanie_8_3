c262ed4ba7e58d816b15cbdc1c3ea94f67daf4bb
Michal Bandyszak <michalbandyszak@gmail.com>
2018-04-14 12:57:17 +0200

    Change .js file -> add comments.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-var a;
+var name = prompt("Enter your name");
+//alert("Hello, " + name);
+console.log("Hi, " + name);
+
+/*==========kometarz blokowy
+Lorem ipsum blah blah blah====*/
+
+//Jedna linia komentarza
+
